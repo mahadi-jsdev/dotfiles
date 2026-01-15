9412647ad8d795126c81d31a2550e8a7730921a4
Mahadi Hasan <mahadi.jsdev@gmail.com>
2026-01-15 10:21:51 +0600

    Add 'image' option to Snacks configuration
    Added 'image' option to Snacks configuration.

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,75 +1,76 @@
 return {
-	"folke/snacks.nvim",
-	lazy = false,
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-		lazy = true,
-	},
-	opts = {
-		picker = {},
-		bigfile = {},
-		quickfile = {},
-		bufDelete = {},
-		statuscolumn = {},
-		scope = {},
-		input = {},
-		notifier = {},
-		indent = {},
-		explorer = {},
-		dashboard = {},
-		scratch = {},
-	},
-	keys = {
-		{
-			"<leader><leader>",
-			function()
-				Snacks.picker.files()
-			end,
-			desc = "search files",
-		},
-		{
-			"<C-space>",
-			function()
-				Snacks.picker.buffers()
-			end,
-			desc = "buffers",
-		},
-		{
-			"<leader>e",
-			function()
-				Snacks.explorer()
-			end,
-			desc = "explorer",
-		},
-		{
-			"<C-f>",
-			function()
-				Snacks.picker.grep()
-			end,
-			desc = "Grep",
-			mode = { "n" },
-		},
-		{
-			"<C-f>",
-			function()
-				Snacks.picker.grep_word()
-			end,
-			desc = "Visual selection or word",
-			mode = { "x" },
-		},
-		{
-			"<C-g>",
-			function()
-				Snacks.picker.git_status()
-			end,
-			desc = "Git Status",
-		},
-		{
-			"<C-x>",
-			function()
-				Snacks.bufdelete()
-			end,
-			desc = "Delete Buffer",
-		},
-	},
+  "folke/snacks.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    lazy = true,
+  },
+  opts = {
+    picker = {},
+    bigfile = {},
+    quickfile = {},
+    bufDelete = {},
+    statuscolumn = {},
+    scope = {},
+    input = {},
+    notifier = {},
+    indent = {},
+    explorer = {},
+    dashboard = {},
+    scratch = {},
+    image = {}
+  },
+  keys = {
+    {
+      "<leader><leader>",
+      function()
+        Snacks.picker.files()
+      end,
+      desc = "search files",
+    },
+    {
+      "<C-space>",
+      function()
+        Snacks.picker.buffers()
+      end,
+      desc = "buffers",
+    },
+    {
+      "<leader>e",
+      function()
+        Snacks.explorer()
+      end,
+      desc = "explorer",
+    },
+    {
+      "<C-f>",
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = "Grep",
+      mode = { "n" },
+    },
+    {
+      "<C-f>",
+      function()
+        Snacks.picker.grep_word()
+      end,
+      desc = "Visual selection or word",
+      mode = { "x" },
+    },
+    {
+      "<C-g>",
+      function()
+        Snacks.picker.git_status()
+      end,
+      desc = "Git Status",
+    },
+    {
+      "<C-x>",
+      function()
+        Snacks.bufdelete()
+      end,
+      desc = "Delete Buffer",
+    },
+  },
 }
